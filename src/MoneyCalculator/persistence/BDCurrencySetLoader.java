15e709d7c13985bc5772b3ebc9027811af2db1d1
Ayoze <Ayoze@10.230.174.114>
2014-01-21 13:49:18 +0000

    Quitado codigo sobrante de CSL

--- src/MoneyCalculator/persistence/BDCurrencySetLoader.java
+++ src/MoneyCalculator/persistence/BDCurrencySetLoader.java
@@ -11,7 +11,22 @@ public class BDCurrencySetLoader extends CurrencySetLoader{
 
     @Override
     public void load() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //
+//    private static final String URL = "jdbc:oracle:thin:@10.22.146.238:1521:orcl";
+//     private static final String USER = "system";
+//     private static final String PASSWORD = "orcl";
+//
+//     public static void main(String[] args) throws SQLException {
+//     DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+//     Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+//     Statement statement = connection.createStatement();
+//     ResultSet resulSet = statement.executeQuery("SELECT * FROM CAMBIO_EUR_A");
+//     while (resulSet.next()) {
+//     System.out.print(resulSet.getString("DIVISA") + " ");
+//     System.out.println(resulSet.getBigDecimal("CAMBIO"));
+//     }
+//     connection.close();
+//     }w UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
